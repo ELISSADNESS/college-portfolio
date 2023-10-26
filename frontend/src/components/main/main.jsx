660ab817be07bf2d712f81793d6cf65285f4ed0f
Elizaveta <eliza85d@gmail.com>
2023-10-26 12:54:36 +0700

    26.10.23

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -9,12 +9,12 @@ function Main(){
         <>
             <div className={styles.container}>
                 <div className={styles.works}>
+                    <img src="https://images.unsplash.com/photo-1549277513-f1b32fe1f8f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" />
+                    <img src="https://images.unsplash.com/photo-1631479290037-97d776982ea4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2008&q=80" />
+                    <img src="https://images.unsplash.com/photo-1598349326101-fc9e68975a52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" />
+                    <img src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80" />
                     <img src="" />
-                    <img src="" />
-                    <img src="" />
-                    <img src="" />
-                    <img src="" />
-                    <img src="" />
+                    <img src="https://images.unsplash.com/photo-1566404252805-1e6d6bc539d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" />
                 </div>
             </div>
             <div className={styles.btnCenter}>
